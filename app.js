55226d38abf2729368a966310e74d86c6cce6f53
Darshan Patni <darshan1695@gmail.com>
2020-09-13 22:51:43 -0400

    pump mouse over

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var MAP_HEIGHT = 20;
+var PUMP_RADIUS = 3;
+var PUMP_COLOR = "orange";
 
 var mapSVGContainer = d3.select("div")
     .append("svg")
@@ -31,22 +33,50 @@ var pumpSVGContainer = d3.select("div")
     .attr("preserveAspectRatio", "xMinYMin meet")
     .attr("viewBox", "0 0 1000 400")
     .classed("svg-content-responsive", true);
-    
+
 var circles = pumpSVGContainer.selectAll("circle")
                         .append("circle");
                         
 function drawPumps(item) {
-    var circles = pumpSVGContainer
-                        .append("circle")
-                        .attr("cx", MAP_HEIGHT*item.x)
-                        .attr("cy", MAP_HEIGHT*item.y)
-                        .attr("r", 2)
-                        .attr("fill", "red")
-                        .style("fill", "red")
-                        .attr("width", 600)
-                        .attr("height", 400);
-}
+    var g = pumpSVGContainer
+        .append("g")
+        .attr("transform", function(d) {
+            return "translate(" + MAP_HEIGHT*item.x + ","+ MAP_HEIGHT*item.y +")" ;
+        })
+        // .on("mouseover", function(){return handleMouseOver(this, item);})
+        // .on("mouseout", function(){return handleMouseOut(this, item);});
 
+        g.append("circle")
+        .attr("r", PUMP_RADIUS)
+        .attr("fill",PUMP_COLOR)
+        .attr("width", 600)
+        .attr("height", 400)
+    }
+
+// Create Event Handlers for mouse
+function handleMouseOver(g,d) {  // Add interactivity
+    // Use D3 to select element, change color and size
+    d3.select(g).select("circle").attr({
+        fill: "orange",
+        r: PUMP_RADIUS * 2
+    });
+
+    // Specify where to put label of text
+    d3.select(g)
+    .append("text")
+    .text("Pump");
+    }
+
+function handleMouseOut(g,d) {
+    // Use D3 to select element, change color back to normal
+    d3.select(g).select("circle").attr({
+        fill: PUMP_COLOR,
+        r: PUMP_RADIUS
+    });
+
+    // Select text by id and then remove
+    d3.select(g).selectAll("text").remove();  // Remove text location
+    }
 d3.csv("knowledge/pumps.csv", function(data) {
     data.forEach(drawPumps);
 });
